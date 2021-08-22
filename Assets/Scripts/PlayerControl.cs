ceb76269ec4d86b1e9866d8387a47e469bcfe36d
Cleosetric <col.hackerz2@gmail.com>
2021-08-22 01:41:38 -0700

    Add Player Controller

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    public float powerShoot = 10f;
+    private Rigidbody2D rb;
+    private PlayerBall player;
+    private Camera cam;
+
+    public Vector2 minPower;
+    public Vector2 maxPower;
+    private Vector2 force;
+    private Vector3 startPoint;
+    private Vector3 endPoint;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam = Camera.main;
+        player = FindObjectOfType<PlayerBall>();
+        rb = player.gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0)){
+            startPoint = cam.ScreenToWorldPoint(Input.mousePosition);
+            startPoint.z = 15;
+        }
+
+        if(Input.GetMouseButton(0)){
+            Vector3 currentPoint = cam.ScreenToWorldPoint(Input.mousePosition);
+            currentPoint.z = 15;
+        }
+
+        if(Input.GetMouseButtonUp(0)){
+            endPoint = cam.ScreenToWorldPoint(Input.mousePosition);
+            endPoint.z = 15;
+            force = new Vector2(Mathf.Clamp(startPoint.x - endPoint.x,minPower.x,maxPower.x),
+            Mathf.Clamp(startPoint.y - endPoint.y,minPower.y,maxPower.y));
+            rb.AddForce(force * powerShoot, ForceMode2D.Impulse);
+        }
     }
 }
